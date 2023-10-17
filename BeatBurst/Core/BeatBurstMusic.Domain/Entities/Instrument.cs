b12dd4d995a9a8a225bc658cd7f9d37d1ddbf8d1
Cengizhan <cengizhancivelek@gmail.com>
2023-10-17 23:42:55 +0300

    Add EntityBase class for common properties and entities.

---
+++ BeatBurst/Core/BeatBurstMusic.Domain/Entities/Instrument.cs
@@ -0,0 +1,26 @@
+﻿using BeatBurstMusic.Domain.Common;
+using BeatBurstMusic.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeatBurstMusic.Domain.Entities
+{
+    public class Instrument : EntityBase<Guid>
+    {
+        public string Name { get; set; }
+
+        public Brand Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public Color Color { get; set; }
+
+        public DateTime? ProductionYear { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}
