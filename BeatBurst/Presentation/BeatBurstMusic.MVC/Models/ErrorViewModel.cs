d1b61ad7a7b5c7c3369bc0e910120ae0cb25771d
Cengizhan <cengizhancivelek@gmail.com>
2023-10-17 23:25:41 +0300

    Project has been created

---
+++ BeatBurst/Presentation/BeatBurstMusic.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeatBurstMusic.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
